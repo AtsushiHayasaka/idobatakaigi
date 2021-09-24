8b5ddc06bb345e4f8c308767829728671e3d753a
AtsushiHayasaka <a.hayusaka0614@gmail.com>
2021-09-24 14:01:46 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import SignIn from './SignIn';
 import Main from './Main';
+import config from '../config.json';
+
+console.log(config);
 
 export default () => {
   const [name, setName] = useState('');
   console.log({ name });
 
-  if (name === '') {
+  if (name === '' && config.signInEnabled) {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
