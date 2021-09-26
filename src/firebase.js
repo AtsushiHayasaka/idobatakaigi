4e9f835a6700ef51033e829337ec85abf4ba9b88
AtsushiHayasaka <a.hayusaka0614@gmail.com>
2021-09-26 17:31:15 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,28 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCIJP17RgCWfakT0g89L87Oi7rh4BkIFc8',
-  authDomain: 'idobatakaigi-with-ham-2debd.firebaseapp.com',
-  projectId: 'idobatakaigi-with-ham-2debd',
-  storageBucket: 'idobatakaigi-with-ham-2debd.appspot.com',
-  messagingSenderId: '62135170037',
-  appId: '1:62135170037:web:367ca50238aa39fb8966bc',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text }) => {
-  messageRef.push({ name, text });
+  messagesRef.push({ name, text });
 };
